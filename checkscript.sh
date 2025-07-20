4b1efc7bc632f1bdb67d66a880d99987c0ef2735
Astronaut701 <39848073+astronaut701@users.noreply.github.com>
2025-07-20 12:57:41 -0400

    Switch from sudo to su to correct an issue with log file permissions

--- checkscript.sh
+++ checkscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Pathes to the scripts
+# Path to the agent script
 AGENT_SCRIPT_PATH="/opt/monitor/agent.py"
 COLLECTOR_SCRIPT_PATH="/opt/monitor/collector.py"
 # The user to run the agent as
@@ -13,15 +13,14 @@ if ! pgrep -f -u "$RUN_AS_USER" "$AGENT_SCRIPT_PATH" > /dev/null; then
     # The 'nohup' and '&' ensure the script runs in the background
     # and continues running after this script exits.
     # Redirect output to a log file.
-    sudo -u "$RUN_AS_USER" nohup python3 "$AGENT_SCRIPT_PATH" > /opt/monitor/agent.log 2>&1 &
+    su - "$RUN_AS_USER" -s /bin/bash -c \ "exec /usr/bin/nohup python3 $AGENT_SCRIPT_PATH > /opt/monitor/agent.log 2>&1 < /dev/null &"
 else
     echo "Agent is running."
 fi
 
-# Check if the process is running
 if ! pgrep -f -u "$RUN_AS_USER" "$COLLECTOR_SCRIPT_PATH" > /dev/null; then
     echo "Collector process not found. Restarting..."
-    sudo -u "$RUN_AS_USER" nohup python3 "$COLLECTOR_SCRIPT_PATH" > /opt/monitor/collector.log 2>&1 &
+    su - "$RUN_AS_USER" -s /bin/bash -c \ "exec /usr/bin/nohup python3 $COLLECTOR_SCRIPT_PATH > /opt/monitor/collector.log 2>&1 < /dev/null &"
 else
     echo "Collector is running."
 fi
